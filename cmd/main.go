c1fe805e7134a1be77ef38c00baf764e45406691
Tanya <tetianaha@wizardsdev.com>
2023-08-05 11:03:32 +0300

    add struct project

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"Auth"
+	"Auth/pkg/handler"
+	"log"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+
+	srv := new(Auth.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
